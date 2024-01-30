7f382ee808b0285593fc82a5d57f9119b1e119b4
Samwes13 <sampo.westerholm@gmail.com>
2024-01-30 18:11:39 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const[product, setProduct] = useState('');
+  const [productList, setProductList] = useState([]);
+
+  const addProduct = () => {
+    setProductList([...productList, {key: product}])
+    setProduct("");
+  };
+
+  const clearAll = () => {
+    setProductList([]);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        value={product}
+        onChangeText={text => setProduct(text)}
+        />
+
+      <View style={{flexDirection: 'row',
+            alignItems: 'center'}}>
+      <Button title='ADD' onPress={addProduct}/>
+      <Button title='CLEAR' onPress={clearAll}/>
+      </View>
+      <Text style={{fontSize: 16, margin: 11}}>Shopping List</Text>
+      <FlatList style={styles.list}
+        data={productList}
+        renderItem={({ item }) =>
+          <Text>{item.key}</Text>
+        }
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +49,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 100,
   },
 });
